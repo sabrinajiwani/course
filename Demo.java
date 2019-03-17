f85e185404c3343107499b39f7484da4ba8f172b
sjiwani <sabrina.jiwani@gmail.com>
2019-03-18 04:04:26 +0530

    Testing commits of 2 files together with out -a

--- Demo.java
+++ Demo.java
@@ -1,5 +1,6 @@
-total 12
-drwxr-xr-x  3 edureka edureka 4096 Mar 18 03:26 .
-drwxr-xr-x 29 edureka edureka 4096 Mar 18 03:25 ..
--rw-r--r--  1 edureka edureka    0 Mar 18 03:26 Demo.java
-drwxr-xr-x  7 edureka edureka 4096 Mar 18 02:25 .git
+total 16
+drwxr-xr-x  3 edureka edureka 4096 Mar 18 04:00 .
+drwxr-xr-x 30 edureka edureka 4096 Mar 18 03:51 ..
+-rw-r--r--  1 edureka edureka   21 Mar 18 03:26 Demo.class
+-rw-r--r--  1 edureka edureka    0 Mar 18 04:01 Demo.java
+drwxr-xr-x  8 edureka edureka 4096 Mar 18 04:00 .git
